ad99c9753443d534c0117bdd008e7a6dfe02d6be
Nuh <118152792+NuhShh@users.noreply.github.com>
2024-11-20 23:47:16 +0700

    Add Folder Spring Boot Backend

---
+++ swanlake-tech-backend/src/main/java/com/tubes/swanlake_tech_backend/SwanlakeTechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tubes.swanlake_tech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwanlakeTechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwanlakeTechBackendApplication.class, args);
+	}
+
+}
---
+++ swanlake-tech-backend/src/test/java/com/tubes/swanlake_tech_backend/SwanlakeTechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubes.swanlake_tech_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwanlakeTechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
