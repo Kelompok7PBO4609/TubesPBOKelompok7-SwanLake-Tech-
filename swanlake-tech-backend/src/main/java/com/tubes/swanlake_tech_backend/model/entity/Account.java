1512d1a757ef45875b874c516f4be5f457fae65a
NuhShh <nhp5810@gmail.com>
2024-12-02 01:15:05 +0700

    Update code entity, membuat service, dto, controller, tes sementara untuk registrasi akun

--- swanlake-tech-backend/src/main/java/com/tubes/swanlake_tech_backend/model/entity/Account.java
+++ swanlake-tech-backend/src/main/java/com/tubes/swanlake_tech_backend/model/entity/Account.java
@@ -15,7 +15,11 @@ public abstract class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long accountID;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
     private String password;
 
     public Account(String username, String password) {
