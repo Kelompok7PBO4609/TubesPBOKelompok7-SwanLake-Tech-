e1ceee7802caaed9f9bb0590b4c0dc4f0332ff29
NuhShh <nhp5810@gmail.com>
2024-12-12 19:13:56 +0700

    Buat halaman userlist untuk cek integrasi antara FE dan BE menggunakan axios

--- swanlake-frontend/src/App.tsx
+++ swanlake-frontend/src/App.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import HomePage from './pages/HomePage';
-import CategoryPage from './pages/CategoryPage';
-import ReviewPage from './pages/ReviewPage';
-import { ThemeProvider } from './Theme/ThemeSwitcher';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CategoryPage from "./pages/CategoryPage";
+import ReviewPage from "./pages/ReviewPage";
+import { ThemeProvider } from "./Theme/ThemeSwitcher";
 import TopRatedReviewsPage from "./pages/TopRatedPage";
 import TrendingReviewsPage from "./pages/TrendingReviews";
+import UserList from "./pages/UserList";
 
 export default function App() {
   return (
-      <ThemeProvider>
-        <Router>
-          <div className="min-h-screen transition-colors duration-200 dark:bg-gray-900">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/category/:category" element={<CategoryPage />} />
-              <Route path="/review/:id" element={<ReviewPage />} />
-                <Route path="/top-rated/" element={<TopRatedReviewsPage />} />
-                <Route path="/trending-review/" element={<TrendingReviewsPage />} />
-            </Routes>
-          </div>
-        </Router>
-      </ThemeProvider>
+    <ThemeProvider>
+      <Router>
+        <div className="min-h-screen transition-colors duration-200 dark:bg-gray-900">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/category/:category" element={<CategoryPage />} />
+            <Route path="/review/:id" element={<ReviewPage />} />
+            <Route path="/top-rated/" element={<TopRatedReviewsPage />} />
+            <Route path="/trending-review/" element={<TrendingReviewsPage />} />
+            <Route path="/userlist/" element={<UserList />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
